a6c2fa8a128979a072c5bb2be1b59f73115c883d
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-06-20 16:47:51 -0400

    update

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,21 +2,30 @@ import React from 'react'
 import {Suspense, useEffect, useState} from 'react'
 import {Canvas} from '@react-three/fiber'
 import {OrbitControls, Preload, preload, useGITF} from '@react-three/drei'
-import CanvasLoader from './Loader'
-import { ComputersCanvas } from '.'
+import CanvasLoader from '../Loader'
+import { ComputersCanvas } from '../canvas'
 
 const Computers = () => {
   const computer = useGLTF('/desktop_pc/scene.gltf')
 
   return (
-
     <mesh>
+      <hemisphereLight intensity = {0.15}
+      groundColor= "black" />
+      <pointLight intensity={1} />
+      <spotLight position = {[-20, 50, 10]}
+      angle = {0.12}
+      penumbra = {1}
+      intensity = {2}
+      castShadow
+      shadow-mapSize={1024}/>
 
-      <hemisphereLight intensity = {0.15} groundColor= "black" />
-      <pintLight intensity={1} />
       <primitive
       object = {computer.scene}
-
+      scale = {0.75}
+      position = {[0, -3.25, -1.5]}
+      rotation = {[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   )
 }
@@ -38,5 +47,6 @@ const computersCanvas = () => {
         <Preload all />
     </Canvas>
   )
+}
 
-export default ComputersCanvas
+export default ComputersCanvas;
