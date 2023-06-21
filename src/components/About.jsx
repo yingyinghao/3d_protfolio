37fd4de8183bb159cf6d334a1f36a2f1961c29d6
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-06-21 10:55:21 -0400

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import Tilt from "react-parallax-tilt"
+import {motion} from 'framer-motion'
+import {styles} from '../styles'
+import { services } from '../constants'
+import {fadeIn, textVariant} from '../utils/motion';
+import { fade } from 'maath/dist/declarations/src/misc'
+
+
+const ServicesCard = ({ index, title, icon}) => {
+  return (
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
+      </motion.div>
+ </Tilt>
+      )
+}
+
+
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <motion.div variants={textVariant()}>
+    <p className = {styles.sectionSubText}>Introduction</p>
+    <h2 className={styles.sectionHeadText}>Overview</h2>
+
+
+    </motion.div>
+    <motion.p variants={fadeIn("", "", 0.1, 1)} className='mt-4 text-secondary text-[17px] max-w-3xl leading-[3-px]'>
+ I am skillful web developer with experience in js and react!
+
+    </motion.p>
+
+<div className='mt-20 flex flex-wrap gap-10'>
+  {services.map((service, index) => (
+    <ServicesCard key = {service.title} index={index} {...service} />
+  ))}
+</div>
+    </>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,10 +3,13 @@ import {Suspense, useEffect, useState} from 'react'
 import {Canvas} from '@react-three/fiber'
 import {OrbitControls, Preload, preload, useGITF} from '@react-three/drei'
 import CanvasLoader from '../Loader'
-import { ComputersCanvas } from '../canvas'
+
+const Computers = (( isMobile )) => {
 
 
-const Computers = () => {
+}
+
+
   const computer = useGLTF('/desktop_pc/scene.gltf')
 
   return (
@@ -23,8 +26,8 @@ const Computers = () => {
 
       <primitive
       object = {computer.scene}
-      scale = {0.75}
-      position = {[0, -3.25, -1.5]}
+      scale = { isMobile ? 0.7 : 0.75}
+      position = {isMobile ? [0, -3, -2.2] :[0, -3.25, -1.5]}
       rotation = {[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -32,7 +35,27 @@ const Computers = () => {
 }
 
 
-const computersCanvas = () => {
+const ComputersCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(`max-width:500`);
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    mediaQuery.addEventListener(`change`, handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener(`change`, handleMediaQueryChange), []
+    }
+
+
+
   return (
     <Canvas
     frameLoop = "demand"
@@ -42,7 +65,7 @@ const computersCanvas = () => {
     >
       <Suspense fallback = {<CanvasLoader />}>
         <OrbitControls enableZoom = {false} maxPolarAngle = {Math.PI / 2} minPolarAngle = {Math.PI / 2} />
-        <Computers />
+        <Computers isMobile = {isMobile} />
         </Suspense>
 
         <Preload all />
