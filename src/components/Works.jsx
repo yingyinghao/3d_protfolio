0493cba4cc9ce911dea8e63cb50cdb2b112697cb
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-06-23 00:16:35 -0400

    ball

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import Tilt from "react-parallax-tilt"
+import {motion} from 'framer-motion'
+import {styles} from '../styles'
+import { github } from '../assets'
+import {SectionWrapper} from '../hoc'
+import { projects } from "../constants"
+import {fadeIn, textVariant} from '../utils/motion'
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+    <motion.div variants={textVariant()}>
+    <p className = {styles.sectionSubText}>Works</p>
+    <h2 className={styles.sectionHeadText}>My Projects</h2>
+    </motion.div>
+
+    <div className='w-full flex'>
+      <motion.p variants={fadeIn("", "", 0.1, 1)} className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+        Here are some of my projects that I have worked on. You can find more on my github.
+      </motion.p>
+      </div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "")
