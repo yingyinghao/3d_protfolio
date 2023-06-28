f1e3814b2db38b25a3a9d27bdd12c3aeaea81d17
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-06-28 14:57:25 -0400

    Earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -22,8 +22,16 @@ const Earth = () => {
 
 const EarthCanvas = () => {
   return (
-    <Canvas shadows frameloop='demand' gl={{ preserveDrawingBuffer: true}}
-   camera={{}}
+    <Canvas
+    shadows
+    frameloop='demand'
+     gl={{ preserveDrawingBuffer: true}}
+   camera={{
+    fov:45,
+    near: 0.1,
+    far:200,
+    position: [-4, 3, 6]
+  }}
    >
     <Suspense fallback={<CanvasLoader />}>
       <OrbitControls
