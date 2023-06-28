2b3bf802d6c1c95f753d1152ce956b52b9ed9af9
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-06-27 23:53:11 -0400

    Earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import {Suspense} from 'react'
+import {Canvas} from '@react-three/fiber'
+import {OrbitControls, Stars} from '@react-three/drei'
+import CanvasLoader from '../Loader'
+
+
 
 const Earth = () => {
   return (
     <div>Earth</div>
+  )
+}
+
+const EarthCanvas = () => {
+  return (
+    <Canvas shadows frameloop='demand' gl={{ preserveDrawingBuffer: true}}
+   camera={{}}
+   >
+    <Suspense fallback={<CanvasLoader />}>
+      <OrbitControls
+      outoRotate
+      enablePan={false}
+      maxPolarAngle={Math.PI / 2}
+      minPolarAngle={Math.PI / 2}
+      />
+      <Earth/>
+      </Suspense>
+
+    </Canvas>
   )
 }
 
